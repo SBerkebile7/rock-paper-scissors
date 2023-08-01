3f9127c92be66363163247204f04067c291acb74
Spencer Berkebile <sberkebile@live.com>
2023-08-01 17:29:16 -0400

    basic additions to function, tried to create switch functionality for computer selection

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function getComputerChoice() {
-
+    let computerChoice = 0;
+    switch(Math.floor(Math.random() * 3) + 1) {
+        case 1:
+            computerChoice = 'rock'
+            break;
+        case 2:
+            computerChoice = 'paper'
+            break;
+        case 3:
+            computerChoice = 'scissors'
+            break;
+    }
+    return computerChoice;
 }
 
 function singleRound(playerSelection, computerSelection) {
@@ -9,3 +21,6 @@ function singleRound(playerSelection, computerSelection) {
 function game() {
 
 }
+
+const computerSelection = getComputerChoice();
+let playerSelection = prompt("Do you choose rock, paper or scissors?");
